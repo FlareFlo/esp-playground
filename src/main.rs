42fbee1fcdafe39710ebcff170709b5c96f799a1
FlareFlo <mail@flareflo.dev>
2023-09-03 19:15:21 +0200

    Init wifi setup

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@
 extern crate alloc;
 use esp_backtrace as _;
 use esp_println::println;
+use esp_wifi::wifi::WifiMode;
 use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc, IO, Delay};
 use hal::i2c::I2C;
+use hal::systimer::SystemTimer;
+
+const SSID: &str = env!("SSID");
+const PASSWORD: &str = env!("PASSWORD");
 
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
@@ -53,27 +58,49 @@ fn main() -> ! {
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     println!("Setup complete");
 
-    let  i2c = I2C::new(
-        peripherals.I2C0,
-        io.pins.gpio6,
-        io.pins.gpio7,
-        100u32.kHz(),
-        &mut system.peripheral_clock_control,
-        &clocks,
-    );
-    let delay = Delay::new(&clocks);
+    // Wifi stuff
+    {
+        let timer = SystemTimer::new(peripherals.SYSTIMER).alarm0;
+        let init = esp_wifi::initialize(
+            esp_wifi::EspWifiInitFor::Wifi,
+            timer,
+            hal::Rng::new(peripherals.RNG),
+            system.radio_clock_control,
+            &clocks,
+        ).unwrap();
+        let (wifi, _) = peripherals.RADIO.split();
 
-    let mut aht20 = aht20::Aht20::new(i2c, delay).unwrap();
-
-    let mut loop_delay = Delay::new(&clocks);
-
-    aht20.reset().unwrap();
-    aht20.calibrate().unwrap();
-
-    while let Ok((humidity, temperature)) = aht20.read() {
-        println!("Temp {:.1}% {:.1}°C", humidity.rh(), temperature.celsius());
-        loop_delay.delay_ms(1000u32);
+        let (interface, controller) = esp_wifi::wifi::new_with_mode(
+                &init,
+                wifi,
+            WifiMode::Sta
+        ).unwrap();
     }
+
+    // Measurement stuff
+    // {
+    //     let  i2c = I2C::new(
+    //         peripherals.I2C0,
+    //         io.pins.gpio6,
+    //         io.pins.gpio7,
+    //         100u32.kHz(),
+    //         &mut system.peripheral_clock_control,
+    //         &clocks,
+    //     );
+    //     let delay = Delay::new(&clocks);
+    //
+    //     let mut aht20 = aht20::Aht20::new(i2c, delay).unwrap();
+    //
+    //     let mut loop_delay = Delay::new(&clocks);
+    //
+    //     aht20.reset().unwrap();
+    //     aht20.calibrate().unwrap();
+    //
+    //     while let Ok((humidity, temperature)) = aht20.read() {
+    //         println!("Temp {:.1}% {:.1}°C", humidity.rh(), temperature.celsius());
+    //         loop_delay.delay_ms(1000u32);
+    //     }
+    // }
 
     loop {}
 }
